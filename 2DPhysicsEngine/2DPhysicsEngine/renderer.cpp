386434a3f5c1db50ed5b97059880ec11f8177129
ZackMisso <zack441@mac.com>
2015-01-05 20:35:36 -0500

    Trying to learn OpenGL. The tutorial I was following used a deprecated version of GLUT so now I need to find another

--- 2DPhysicsEngine/2DPhysicsEngine/renderer.cpp
+++ 2DPhysicsEngine/2DPhysicsEngine/renderer.cpp
@@ -7,3 +7,61 @@
 //
 
 #include "renderer.h"
+
+void RenderingEngine::createWindow(){
+    // implement
+}
+
+void RenderingEngine::render2DParticle(Particle2D particle){
+    // implement
+}
+
+void RenderingEngine::render3DParticle(Particle3D particle){
+    // implement
+}
+
+void RenderingEngine::renderCircle(Circle circle){
+    // implement
+}
+
+void RenderingEngine::init(){
+    glGenVertexArrays(NumVAOs,VAOs);
+    glBindVertexArray(VAOs[Triangles]);
+    
+    GLfloat vertices[NumVertices][2] = {
+        { -.9, -.9 }, // triangle 1
+        { .85, -.9 },
+        { -.9, .85 },
+        { .9, .85 }, // triangle 2
+        { .9, .9 },
+        { -.85, .9 }
+    };
+    
+    glGenBuffers(NumBuffers,Buffers);
+    glBindBuffer(GL_ARRAY_BUFFER,Buffers[ArrayBuffer]);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    ShaderInfo shaders[] = {
+        { GL_VERTEX_SHADER,"triangles.vert" },
+        { GL_FRAGMENT_SHADER,"triangles.frag" },
+        { GL_NONE,0x0 }
+    };
+    
+    GLuint program = LoadShaders(shaders);
+    glUseProgram(program);
+    glVertexAttribPointer(vPosition,2,GL_FLOAT,GL_FALSE,0,0x0);
+    //glVer
+    glEnableVertexAttribArray(vPosition);
+}
+
+void RenderingEngine::display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(VAOs[Triangles]);
+    glDrawArrays(GL_TRIANGLES,0,NumVertices);
+    glFlush();
+}
+
+void RenderingEngine::test(int argc,char** argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(512,512);
+}
--- 2DPhysicsEngine/2DPhysicsEngine/renderer.h
+++ 2DPhysicsEngine/2DPhysicsEngine/renderer.h
@@ -9,20 +9,40 @@
 #ifndef ___DPhysicsEngine__renderer__
 #define ___DPhysicsEngine__renderer__
 
+#ifdef __APPLE__
+#define glGenVertexArrays glGenVertexArraysAPPLE
+#define glBindVertexArray glBindVertexArrayAPPLE
+#define glBindVertexArrays glBindVertexArraysAPPLE
+#define glDeleteVertexArrays glDeleteVertexArraysAPPLE
+#define BUFFER_OFFSET(offset) ((GLvoid*)(offset));
+#endif
+
 #include <stdio.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#include <OpenGL/gliContext.h>
+#include <OpenGL/OpenGL.h>
 #include "particle.h"
 #include "aabb.h"
 #include "circle.h"
 
 class RenderingEngine{
     // Figure out how to use this
+    enum VAO_IDs { Triangles, NumVAOs }; // learn
+    enum Buffer_IDs { ArrayBuffer, NumBuffers }; // learn
+    enum Attrib_IDs { vPosition = 0 }; // learn
+    static GLuint VAOs[NumVAOs];
+    static GLuint Buffers[NumBuffers];
+    static const GLuint NumVertices = 6;
+    static void createWindow();
     static void render2DParticle(Particle2D particle);
     static void render3DParticle(Particle3D particle);
     static void renderAABB(AABB aabb);
     static void renderCircle(Circle circle);
+    static void init();
+    static void display();
+    static void test(int argc,char** argv);
 };
 
 #endif /* defined(___DPhysicsEngine__renderer__) */
